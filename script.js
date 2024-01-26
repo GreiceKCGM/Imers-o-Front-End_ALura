fe7887f6ecf6ac3937f6aa65e06c4c4773b02d4d
GreiceKCGM <greicekellybc@gmail.com>
2024-01-26 18:38:20 -0300

    correções do código do js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+console.log("Funcionou")
+
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlist');
+const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResult(result))
+        .then((result) => displayResults(result))
 }
 
-function displayResult(result) {
-    resultPlaylist.classList.add('hidden')
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
@@ -20,7 +22,7 @@ function displayResult(result) {
         
     });
 
-    resultArtist.classListremove('hiden');
+    resultArtist.classList.remove('hidden');
 }
 
 document.addEventListener('input', function(){
@@ -32,5 +34,5 @@ document.addEventListener('input', function(){
     }
 
     requestApi(searchTerm);
-    
+
 })
